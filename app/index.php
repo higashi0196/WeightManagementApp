f802ae91efdfdcb2b091a070972f775b807dc2cf
higashi1000 <nsantamaria100gou@gmail.com>
2022-03-09 18:34:20 +0900

    First commit

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+   <meta charset="utf-8">
+   <title>体重管理アプリ</title>
+   <h1>目標体重</h1>
+</head>
+<body>
+   <h1>今日のToDoリスト</h1>
+   <h1>継続するToDoリスト</h1>
+   <h1>明日への一言</h1>
+</body>
+</html>
