7063d6588c47178d494aeb94573e86c937c3d68e
higashi1000 <nsantamaria100gou@gmail.com>
2022-03-17 18:32:08 +0900

    commit9

--- app/index.php
+++ app/index.php
@@ -4,14 +4,14 @@ echo "アプリ" . PHP_EOL;
 
 <!DOCTYPE html>
 <html lang="ja">
-<head> 
-   <meta charset="utf-8">
-   <title>体重管理アプリ</title>
-   <h1>目標体重</h1>
-</head>
-<body>
-   <h1>今日のToDoリスト</h1>
-   <h1>継続するToDoリスト</h1>
-   <h1>明日への一言</h1>
-</body>
+   <head> 
+      <meta charset="utf-8">
+      <title>体重管理アプリ</title>
+      <h1>目標体重</h1>
+   </head>
+   <body>
+      <h1>今日のToDoリスト</h1>
+      <h1>継続するToDoリスト</h1>
+      <h1>明日への一言</h1>
+   </body>
 </html> 
