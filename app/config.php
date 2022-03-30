316c941d4f3d9a08375eb479902ade24694da0fd
higashi1000 <nsantamaria100gou@gmail.com>
2022-03-30 19:25:20 +0900

    commit23” git commit -m commit23”

--- app/config.php
+++ app/config.php
@@ -1,10 +1,9 @@
 <?php
 
 define('DSN', 'mysql:host=db;dbname=todoapp;charset=utf8mb4');
-define('DB_USER', 'root');
+define('DB_USER', 'higashi');
 define('DB_PASS', 'Nakanaka3535!');
 define('DB_DATABASE', 'todoapp');
 
 require_once('functions.php');
 
-?>
--- app/functions.php
+++ app/functions.php
@@ -1,10 +1,10 @@
 <?php 
 
-require_once('config.php');
+// require_once('config.php');
 
 function pdo_connect() {
    try {
-      $pdo = new PDO(DSN, DB_USER, DB_PASS,[
+         $pdo = new PDO('DSN', 'DB_USER', 'DB_PASS',[
          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
          PDO::ATTR_EMULATE_PREPARES => false,
