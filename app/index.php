425c882b2c207fdca7d8392de0ba6c0a29d500ab
higashi1000 <nsantamaria100gou@gmail.com>
2022-07-01 16:55:04 +0900

    commit116” git commit -m commit116”

--- app/index.php
+++ app/index.php
@@ -19,76 +19,74 @@ $bodylists = $getller->index3();
    <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
-   <main>
 
-      <h1>体重管理リスト</h1>
-      <div class="miyako">
-         <a href="weight.php" class="ishigaki"><button>体重記入</button></a>
-      </div>
+   <h1>体重管理リスト</h1>
+   <div class="miyako">
+      <a href="weight.php" class="ishigaki"><button>体重記入</button></a>
+   </div>
 
-      <?php foreach ($bodylists as $bodylist): ?>
-         <span style="margin-left:30px" >目標体重 : 
-         <input type="text" value=" <?php echo $bodylist['goalweights']; ?>"> kg</span></br>
-         <span style="margin-left:30px"> 現在の体重 : 
-         <input type="text" value=" <?php echo $bodylist['nowweights']; ?>"> kg</span><br>
-         <span style="margin:0 0 0 30px">目標達成まであと
-         <a id="remaining"><?php echo $bodylist['difference']; ?></a> 
-         <a id="unit">kg</a></span><br>
-         <span style="margin:0 0 0 30px">(<?php echo $bodylist['nowdate']; ?> 現在)</span><br>
+   <?php foreach ($bodylists as $bodylist): ?>
+      <span style="margin-left:30px" >目標体重 : 
+      <input type="text" value=" <?php echo $bodylist['goalweights']; ?>"> kg</span></br>
+      <span style="margin-left:30px"> 現在の体重 : 
+      <input type="text" value=" <?php echo $bodylist['nowweights']; ?>"> kg</span><br>
+      <span style="margin:0 0 0 30px">目標達成まであと
+      <a id="remaining"><?php echo $bodylist['difference']; ?></a> 
+      <a id="unit">kg</a></span><br>
+      <span style="margin:0 0 0 30px">(<?php echo $bodylist['nowdate']; ?> 現在)</span><br>
+   <?php endforeach; ?>
+
+   <div>
+      <a class="miyako">画像アップ</a>
+      <a href="picture.php" class="ishigaki"><button>移動</button></a>  
+   </div>
+
+   <div>
+      <a class="miyako">ToDoリスト</a>
+      <a href="create.php" class="ishigaki"><button>新規登録</button></a>  
+   </div>
+
+   <table>
+      <thead>
+         <tr>
+            <th scope="col">タイトル</th>
+            <th scope="col">目標</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+         </tr>
+      </thead>
+      <tbody>
+         <?php if ($lists): ?>
+            <?php foreach ($lists as $todo):?>
+               <tr>
+                  <td><?php echo $todo['title']; ?></td>
+                  <td><?php echo $todo['content']; ?></td> 
+                  <td><a href="edit.php?todo_id=<?php echo $todo['id']?>" class="editbtn"><button>編集</button></a></td>       
+                  <td class="deletebtn" data-id="<?php echo $todo['id']?>"><button>削除</button></td>
+               </tr> 
+            <?php endforeach; ?>
+         <?php else : ?>
+            <td>Todoなし</td>
+         <?php endif; ?>
+      </tbody>
+   </table> 
+
+   <div>
+      <a class="miyako">明日への一言</a>
+      <a href="post.php" class="ishigaki"><button>投稿する</button></a>
+      <a class="wordbtn" data-id="<?php echo $wordtodo['id']; ?>">
+      <button>削除</button></a>
+   </div>
+
+   <div class="miyako">
+   <?php if ($wordlists): ?>
+      <?php foreach ($wordlists as $wordlist): ?> 
+         <p id="word"><?php echo $wordlist['content']; ?></p>
       <?php endforeach; ?>
-
-      <div>
-         <a class="miyako">画像アップ</a>
-         <a href="picture.php" class="ishigaki"><button>移動</button></a>  
-      </div>
-
-      <div>
-         <a class="miyako">ToDoリスト</a>
-         <a href="create.php" class="ishigaki"><button>新規登録</button></a>  
-      </div>
-
-      <table>
-         <thead>
-            <tr>
-               <th scope="col">タイトル</th>
-               <th scope="col">目標</th>
-               <th scope="col"></th>
-               <th scope="col"></th>
-            </tr>
-         </thead>
-         <tbody>
-            <?php if ($lists): ?>
-               <?php foreach ($lists as $todo):?>
-                  <tr>
-                     <td><?php echo $todo['title']; ?></td>
-                     <td><?php echo $todo['content']; ?></td> 
-                     <td><a href="edit.php?todo_id=<?php echo $todo['id']?>" class="editbtn"><button>編集</button></a></td>       
-                     <td class="deletebtn" data-id="<?php echo $todo['id']?>"><button>削除</button></td>
-                  </tr> 
-               <?php endforeach; ?>
-            <?php else : ?>
-               <td>Todoなし</td>
-            <?php endif; ?>
-         </tbody>
-      </table> 
-
-      <div>
-         <a class="miyako">明日への一言</a>
-         <a href="post.php" class="ishigaki"><button>投稿する</button></a>
-         <a class="wordbtn" data-id="<?php echo $wordtodo['id']; ?>">
-         <button>削除</button></a>
-      </div>
-
-      <div class="miyako">
-      <?php if ($wordlists): ?>
-         <?php foreach ($wordlists as $wordlist): ?> 
-            <p id="word"><?php echo $wordlist['content']; ?></p>
-         <?php endforeach; ?>
-      <?php else : ?>
-         <p>非同期通信成功!</p> 
-      <?php endif; ?> 
-      </div>
-   </main>
+   <?php else : ?>
+      <p>非同期通信成功!</p> 
+   <?php endif; ?> 
+   </div>
 
    <!-- <script src="./js/main.js"></script> -->
    <script src="./js/jquery-3.6.0.min.js"></script>
@@ -188,16 +186,18 @@ $bodylists = $getller->index3();
       
       const remaining = document.getElementById("remaining");
       const unit = document.getElementById("unit");
-      const goal = <?php echo $bodylist['difference']; ?>;
+      const difference = <?php echo $bodylist['difference']; ?>;
+      const goalweight = <?php echo $bodylist['goalweights']; ?>;
+      // const nowweight =<?php echo $bodylist['nowweights']; ?>;
      
-      if (goal <= 0 ) {
-         remaining.textContent =  goal + ' kg';
+      if (difference <= 0 ) {
+         remaining.textContent =  difference + ' kg';
          remaining.classList.add('remaining');
          unit.textContent = '見事達成! やったぜ!' ;
          unit.classList.add('unit');
          console.log("0kg以下,達成");
-      } else if (goal <= 0.5) {
-         remaining.textContent = goal  + ' kg';
+      } else if (0 < goalweight * 0.01) {
+         remaining.textContent = difference + ' kg';
          unit.textContent = 'あともう少し頑張ろう!' ;
          unit.classList.add('unit2');
          console.log("0〜0.5kgの間、もう少し");
