dc9932bbecefc1f609c6ffe72a1fedd62b7039b3
higashi1000 <nsantamaria100gou@gmail.com>
2022-09-22 17:57:13 +0900

    heroku commit

--- app/model/Database.php
+++ app/model/Database.php
@@ -69,6 +69,18 @@ class Database
         $this->comment = $comment;
     }
 
+    public function heroku() {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $db_name = substr($url["path"], 1);
+        $db_host = $url["host"];
+        $user = $url["user"];
+        $password = $url["pass"];
+            
+        $dsn = "mysql:dbname=".$db_name.";host=".$db_host;
+    
+        $pdo=new PDO($dsn,$user,$password,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+    }
+
     // todosテーブルのid取得
     public function todogetid($id) {
         try {
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'efd73ead9785ba0844f92a7de5c29f81efc5434b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'efd73ead9785ba0844f92a7de5c29f81efc5434b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
