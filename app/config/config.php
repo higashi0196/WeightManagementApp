05555ba584235e08c03483493b82603e4d2d6127
higashi1000 <nsantamaria100gou@gmail.com>
2022-09-28 16:40:59 +0900

    heroku new2

--- app/config/config.php
+++ app/config/config.php
@@ -6,7 +6,8 @@ session_start();
 // const USER = 'root';
 // const PASSWORD = 'Nanahigashi10!';
 
-$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+// $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+$db = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $db['dbname'] = ltrim($db['path'], '/');
 $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
 $user = $db['user'];
