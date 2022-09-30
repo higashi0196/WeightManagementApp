9be142a78eb5b7b20321b66ddc4d8d0600879b14
higashi1000 <nsantamaria100gou@gmail.com>
2022-09-30 16:04:32 +0900

    heroku3

--- app/config/config.php
+++ app/config/config.php
@@ -2,14 +2,7 @@
 
 session_start();
 
-const DSN = 'mysql:host=mysql;dbname=todolists;charset=utf8mb4';
-const USER = 'root';
-const PASSWORD = 'Nanahigashi10!';
-
-// $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-// $db['dbname'] = ltrim($db['path'], '/');
-// $user = $db['user'];
-// $password = $db['pass'];
-// $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-
-define ('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+// const DSN = 'mysql:host=mysql;dbname=todolists;charset=utf8mb4';
+// const USER = 'root';
+// const PASSWORD = 'Nanahigashi10!';
+// define ('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
